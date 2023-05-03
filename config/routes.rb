b9a60e5e2189dd5c6e15d0f42086dfc47ef694cf
forest-book <124020491+forest-book@users.noreply.github.com>
2023-05-04 00:45:53 +0900

    [Update]Bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get '/top' => 'homes#top'
+  get '/' => 'homes#top'
+  resources:books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
