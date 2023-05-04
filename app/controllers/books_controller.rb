dd07296a09e2a606e5a7c10867ac047bca4eabc4
forest-book <124020491+forest-book@users.noreply.github.com>
2023-05-05 02:36:29 +0900

    [Update]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,19 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
